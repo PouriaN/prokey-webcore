3d0e092c44e73f78e643ad44fd31c2ada2a8a415
Pouria <purianajaflu@gmail.com>
2021-09-12 07:08:43 +0430

    change nem blockchain to work with chaino

--- src/blockchain/servers/prokey/src/nem/NemBlockchain.ts
+++ src/blockchain/servers/prokey/src/nem/NemBlockchain.ts
@@ -13,7 +13,7 @@ export class NemBlockchain extends ProkeyBaseBlockChain {
 
   constructor(coinName: string = "Nem")
   {
-    super("http://127.0.0.1:50001/api/");
+    super();
     this._coinName = coinName;
   }
 
@@ -24,7 +24,7 @@ export class NemBlockchain extends ProkeyBaseBlockChain {
    */
   public async BroadCastTransaction(data: NemSubmitTransaction): Promise<SubmitTransactionResponse> {
     try {
-      return await this.PostToServer<SubmitTransactionResponse>(`${this._coinName}/transaction/submit`, data);
+      return await this.PostToServer<any>(`Transaction/advanced-send/${this._coinName}/`, data);
     } catch (error) {
       throw new Error("error in submit transaction")
     }
@@ -37,7 +37,7 @@ export class NemBlockchain extends ProkeyBaseBlockChain {
    */
   public async GetAddressInfo(reqAddress: RequestAddressInfo): Promise<NemAccountInfo | null> {
     try {
-      return await this.GetFromServer<NemAccountInfo>(`${this._coinName}/account/${reqAddress.address}`);
+      return await this.GetFromServer<NemAccountInfo>(`address/${this._coinName}/${reqAddress.address}`);
     } catch (error) {
       return null;
     }
@@ -50,10 +50,7 @@ export class NemBlockchain extends ProkeyBaseBlockChain {
    * @returns NemTransactionResponse list of transactions
    */
   public async GetAccountTransactions(accountAddress: string, previousPageHash?: string) : Promise<Array<NemTransactionResponse> | null> {
-    let queryUrl = `${this._coinName}/account/transactions/?accountAddress=${accountAddress}`;
-    if (previousPageHash) {
-      queryUrl += `&hash=${previousPageHash}`
-    }
+    let queryUrl = `address/transactions/${this._coinName}/${accountAddress}/10`;
     let serverResponse = await this.GetFromServer<NemAccountTransactionResponse>(queryUrl);
     if (serverResponse != null && serverResponse.data != null)
     {
